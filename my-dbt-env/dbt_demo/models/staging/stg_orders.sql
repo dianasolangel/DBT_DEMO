75d1729842a1a46b8e5831efcd027609c39bb25f
dianasolangel <dianasolangelfh@gmail.com>
2025-07-07 15:52:42 +0200

    challenge raw_orders

---
+++ my-dbt-env/dbt_demo/models/staging/stg_orders.sql
@@ -0,0 +1,10 @@
+with source as (
+    select * from {{ source('my_dbt_db', 'raw_orders') }}
+),
+renamed as (
+    select
+        id as customer_id,
+        customer as customer_name
+    from source
+)
+select * from renamed
